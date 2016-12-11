840ce18aff37d255569cab8c01748aa2ff1b4d5f
Pat Schloss <pschloss@umich.edu>
2016-12-10 21:43:33 -0500

    Initial commit

---
+++ code/parse_biorxiv.R
@@ -0,0 +1,59 @@
+library(httr)
+library(rvest)
+
+all_files <- list.files('data/dois', full.names=T)
+articles <- all_files[grepl("\\d$", all_files)]
+metrics <- all_files[grepl("article-metrics$", all_files)]
+infos <- all_files[grepl("article-info$", all_files)]
+
+doi <- gsub("data/dois/", "10.1101/", articles)
+
+
+article_page <- read_html(articles[1])
+metric_page <- read_html(metrics[1])
+info_page <- read_html(infos[1])
+
+#authors
+authors <- article_page %>%
+						html_nodes(".highwire-citation-authors") %>%
+						html_text() %>% .[[1]] %>%
+						strsplit(", ") %>% unlist
+
+#first_author
+first_author <- authors[1]
+
+#corr_author
+corr_author <- info_page %>%
+								html_nodes(".contributors li") %>%
+								.[[grep(">1<", .)]] %>%
+								html_nodes(".name") %>%
+								html_text()
+
+#affiliation
+affiliation <- info_page %>%
+								html_nodes("address") %>%
+								html_text() %>%
+								gsub("[\t\n]", "", .) %>%
+								gsub ("^\\s*", "", .)
+
+
+#n_versions
+n_versions <- info_page %>%
+										html_nodes(".hw-version-previous-link") %>%
+										length() + 1
+
+#date_first_deposited
+date_first_deposited <- info_page %>% html_nodes(".publication-history") %>% html_text() %>% .[[2]] %>% gsub("Posted (.*)\\.", "\\1", .)
+
+if(n_versions > 1){
+	date_first_deposited <- info_page %>%
+														html_nodes(".hw-version-previous-link") %>%
+														html_text() %>%
+														gsub("Previous version \\((.*) - .*\\)\\.", "\\1", .)
+}
+
+
+
+#journal_published
+#license
+#category
