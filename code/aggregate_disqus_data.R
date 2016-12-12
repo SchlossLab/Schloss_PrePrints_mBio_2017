2308c61f8172e3f9daf373b48c7417b780193113
Pat Schloss <pschloss@umich.edu>
2016-12-12 10:51:30 -0500

    Initial commit

---
+++ code/aggregate_disqus_data.R
@@ -0,0 +1,33 @@
+#install.packages("rjson")
+library(rjson)
+library(dplyr)
+
+parse_response <- function(x){
+	if(grepl("biorxiv.org", x$link)){
+		paper_id <- gsub(".*/(\\d*\\.?\\d)$", "\\1", x$link)
+		n_posts <- x$posts
+		return(c(paper_id, n_posts))
+	} else {
+		return(c(NA, NA))
+	}
+}
+
+summarize_json <- function(x){
+	json_data <- fromJSON(file=x)$response
+	t(sapply(json_data, parse_response))
+}
+
+count_list <- lapply(list.files("data/disqus", full.names=T), summarize_json)
+count_df <- data.frame(do.call(rbind, count_list), stringsAsFactors=F)
+colnames(count_df) <- c("article_id", "n_comments")
+count_df <- count_df[!is.na(count_df$article_id),]
+
+count_df$n_comments <- as.numeric(count_df$n_comments)
+
+count_df$article_id <- gsub("\\.\\d", "", count_df$article_id)
+
+count_tbl <- count_df %>%
+								group_by(article_id) %>%
+								summarize(n_comments=sum(n_comments))
+
+write.table(count_tbl, file="data/disqus/comment_count.tsv", row.names=F, quote=F, sep='\t')
