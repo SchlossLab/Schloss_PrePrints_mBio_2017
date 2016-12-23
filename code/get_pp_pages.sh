57114e779a6c0a7da952fc557528c062565ebac3
Pat Schloss <pschloss@umich.edu>
2016-12-23 16:39:37 -0500

    Fix path in checking for filenames with .[12].

--- code/get_pp_pages.sh
+++ code/get_pp_pages.sh
@@ -5,7 +5,7 @@
 mkdir -p data/biorxiv
 
 LOCAL_FILE=$1
-
+LOCAL_FILE=data/biorxiv/001776
 DOI_URL=`echo $LOCAL_FILE | sed "s=data/biorxiv/=http://dx.doi.org/10.1101/="`
 
 BIORXIV_URL=`curl -L -o /dev/null --silent --head --write-out '%{url_effective}\n' $DOI_URL`
@@ -14,12 +14,12 @@ phantomjs code/save_page.js ${BIORXIV_URL} > $LOCAL_FILE
 wget -N ${BIORXIV_URL}.article-info -P data/biorxiv/
 wget -N ${BIORXIV_URL}.article-metrics -P data/biorxiv/
 
-if [ -f ${BIORXIV_URL}.[12].article-info ];
+if [ -f ${LOCAL_FILE}.[12].article-info ];
 then
-	mv ${BIORXIV_URL}.[12].article-info ${BIORXIV_URL}.article-info
+	mv ${LOCAL_FILE}.[12].article-info ${LOCAL_FILE}.article-info
 fi
 
-if [ -f ${BIORXIV_URL}.[12].article-metrics ];
+if [ -f ${LOCAL_FILE}.[12].article-metrics ];
 then
-	mv ${BIORXIV_URL}.[12].article-metrics ${BIORXIV_URL}.article-metrics
+	mv ${LOCAL_FILE}.[12].article-metrics ${LOCAL_FILE}.article-metrics
 fi
