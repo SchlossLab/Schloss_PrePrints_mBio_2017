ef2fd7d8094a6ee4ff6a213fed524d5a73709a02
Pat Schloss <pschloss@umich.edu>
2016-12-31 08:20:16 -0500

    Fix DOI base number

--- code/get_asm_altmetric.sh
+++ code/get_asm_altmetric.sh
@@ -4,6 +4,6 @@ SUFFIX=`cat data/asm_doi_urls.tsv | cut -f 5 -d / | cut -f 1`
 
 for DOI in $SUFFIX
 do
-	curl -o data/asm_altmetric/$DOI.json http://api.altmetric.com/v1/doi/10.1101/$DOI?key=992a953baa805c06d19db4c2b6bb1348
-	sleep 0.5s
+	curl -o data/asm_altmetric/$DOI.json http://api.altmetric.com/v1/doi/10.1128/$DOI?key=992a953baa805c06d19db4c2b6bb1348
+	sleep 0.25s
 done
