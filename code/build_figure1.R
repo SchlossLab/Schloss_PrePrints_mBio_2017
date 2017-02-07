c5caef704fa8b2c92075647e714339cc3ea8b4f7
Pat Schloss <pschloss@umich.edu>
2017-02-06 20:48:10 -0500

    Build full figure 1

--- code/build_figure1.R
+++ code/build_figure1.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(cowplot)
+library(sportcolors)
 
 get_quarter <- function(date){
 
@@ -20,9 +22,19 @@ get_quarter <- function(date){
 	return(quarter)
 }
 
-data <- read.table(file="data/processed/biorxiv_data_summary.tsv", header=T, stringsAsFactors=F)
+my_theme <- theme_classic() +
+							theme(legend.title = element_blank(),
+										legend.background = element_blank(),
+										legend.text = element_text(size=6),
+										legend.key.size = unit(0.5, "lines"),
+										axis.title = element_text(size=8),
+										axis.text = element_text(size=6)
+							)
 
-counts <- data %>% filter(!is.na(date_first_deposited)) %>%
+biorxiv <- read.table(file="data/processed/biorxiv_data_summary.tsv", header=T, stringsAsFactors=F)
+biorxiv$journal_published_doi <- tolower(gsub("http://dx.doi.org/", "", biorxiv$journal_published_doi))
+
+counts <- biorxiv %>% filter(!is.na(date_first_deposited)) %>%
 									mutate(year_quarter = paste(gsub(".*, ", "", date_first_deposited),
 				 																			sapply(date_first_deposited, get_quarter))) %>%
 									group_by(year_quarter) %>%
@@ -31,18 +43,115 @@ counts <- data %>% filter(!is.na(date_first_deposited)) %>%
 
 tidied <- gather(counts, key=dataset, value=n_pp, all_n_pp, micro_n_pp)
 
-ggplot(tidied, aes(x=rep(1:nrow(all_data),2), y=n_pp, group=dataset, col=dataset)) +
+time_course <- ggplot(tidied,
+											aes(x=rep(1:length(unique(tidied$year_quarter)),2),
+													y=n_pp,
+													group=dataset,
+													col=dataset)
+										) +
 			geom_rect(aes(xmin=-Inf, xmax=1.5, ymin=-Inf, ymax=Inf), fill="#CCCCCC", color="#CCCCCC") +
 			geom_rect(aes(xmin=5.5, xmax=9.5, ymin=-Inf, ymax=Inf), fill="#CCCCCC", color="#CCCCCC") +
-			geom_line(size=1.5, lineend="round") +
-			labs(x="Year", y="Number of Preprints Posted per Quarter") +
+			geom_line(lineend="round") +
+			labs(x="Year", y="Number of Preprints\nPosted per Quarter") +
 			scale_x_continuous(breaks=c(3.5,7.5,11.5), labels=c("2014", "2015", "2016")) +
 			scale_y_continuous(limits=c(0,1500)) +
-			scale_color_discrete(breaks=c("all_n_pp", "micro_n_pp"),
-													labels=c("All preprints", "Microbiology-affiliated")) +
-			theme_classic() +
-			theme(legend.position = c(0.23,0.92),
-						legend.title = element_blank(),
-						legend.background = element_blank()
-			) +
-			ggsave("figures/Figure1.tiff", width=6, height=4.5, unit="in")
+			scale_color_manual(breaks=c("all_n_pp", "micro_n_pp"),
+													labels=c("All preprints", "Microbiology-affiliated"),
+													values=c("#002c5a", "#ffcb0b"), name=NULL)+
+			my_theme +
+			theme(legend.position = c(0.23,0.92))
+
+
+################################################################################################
+
+
+asm_altmetric <- read.table(file= 'data/asm_altmetric/altmetric_summary.tsv', header=T)
+asm_altmetric$date <- gsub("(\\d\\d\\d\\d)-.*", "\\1", asm_altmetric$date)
+asm_post_2013 <- asm_altmetric[asm_altmetric$date >= 2013,]
+mbio_altmetric <- asm_post_2013[grepl("mbio", asm_post_2013$article_id), "altmetric"]
+
+micro_affiliated <- biorxiv$is_microbiology | biorxiv$category == "Microbiology"
+biorxiv_altmetric <- biorxiv[micro_affiliated, 'altmetric_score']
+
+source <- c(rep("biorxiv", length(biorxiv_altmetric)),
+						rep("mbio", length(mbio_altmetric)))
+altmetric <- c(biorxiv_altmetric, mbio_altmetric)
+
+source_altmetric <- data.frame(source, altmetric)
+source_altmetric$source <- factor(source_altmetric$source, levels=c("mbio", "biorxiv"))
+
+median_alt <- source_altmetric %>%
+											group_by(source) %>%
+											summarize(median=median(altmetric, na.rm=T),
+																n=n()
+											)
+
+j <- c(biorxiv="bioRxiv", mbio="mBio")
+n <- unlist(c(median_alt[median_alt$source=="biorxiv", "n"], median_alt[median_alt$source=="mbio", "n"]))
+
+plot_labels <- lapply(1:2, function(i) {bquote(italic(.(j[i]))*" (N="*.(n[i])*")")})
+
+altmetric_plot <- source_altmetric %>% #filter(source == "biorxiv") %>%
+											ggplot(aes(x=altmetric, fill=source)) +
+											geom_histogram(binwidth=1, alpha=0.5, position="identity") +
+											geom_vline(data=median_alt, aes(xintercept=median, color=source), size=0.5, show.legend=FALSE, alpha=0.5) +
+											scale_y_continuous(expand = c(0.02, 0), limits=c(0,200)) +
+											scale_x_continuous(expand = c(0.02, 0), limits=c(0,100)) +
+											labs(x="Altmetric Impact Score", y="Number of\nPapers or Preprints") +
+											scale_fill_manual(breaks=names(j), labels=plot_labels, values=paste0("#", team_colors("Chicago Cubs")), name=NULL)+
+											scale_color_manual(breaks=names(j), labels=plot_labels, values=paste0("#", team_colors("Chicago Cubs")), name=NULL)+
+											my_theme +
+											theme(legend.position = c(0.8,0.9))
+
+################################################################################################
+
+biorxiv_cited <- read.csv("data/wos_counts/biorxiv_wos.csv", stringsAsFactors=F)
+biorxiv_cited$doi <- tolower(biorxiv_cited$doi)
+biorxiv_cited <- inner_join(biorxiv_cited, biorxiv, by=c("doi"="journal_published_doi"))
+biorxiv_cited_affiliated <- biorxiv_cited[micro_affiliated,]
+cites_2015 <- grep("2015", biorxiv_cited_affiliated$year_month)
+biorxiv_cited_affiliated_2015 <-  biorxiv_cited_affiliated[cites_2015, "times.cited"]
+
+asm_cited <- read.csv("data/wos_counts/asm_wos.csv", stringsAsFactors=F)
+mbio_cited <- asm_cited[grepl("MBIO", asm_cited$doi),]
+mbio_2015 <- grep("201[45]", mbio_cited$date)
+mbio_cited_2015 <- mbio_cited[mbio_2015, "times.cited"]
+
+source <- c(rep("biorxiv", length(biorxiv_cited_affiliated_2015)),
+						rep("mbio", length(mbio_cited_2015)))
+citations <- c(biorxiv_cited_affiliated_2015, mbio_cited_2015)
+source_citations <- data.frame(source, citations, stringsAsFactors=T)
+source_citations$source <- factor(source_citations$source, levels=c("mbio", "biorxiv"))
+
+median_cites <- source_citations %>%
+										group_by(source) %>%
+										summarize(median=median(citations),
+															n=n()
+										)
+
+j <- c(biorxiv="bioRxiv", mbio="mBio")
+n <- unlist(c(median_cites[median_cites$source=="biorxiv", "n"], median_cites[median_cites$source=="mbio", "n"]))
+
+plot_labels <- lapply(1:2, function(i) {bquote(italic(.(j[i]))*" (N="*.(n[i])*")")})
+
+citation_plot <- source_citations %>%
+											ggplot(aes(x=citations, fill=source)) +
+											geom_histogram(binwidth=1, alpha=0.5, position="identity") +
+											geom_vline(data=median_cites, aes(xintercept=median, color=source), size=0.5, show.legend=FALSE, alpha=0.5) +
+											scale_y_continuous(expand = c(0.02, 0), limits=c(0,100)) +
+											scale_x_continuous(expand = c(0.02, 0), limits=c(0,80)) +
+											scale_fill_manual(breaks=names(j), labels=plot_labels, values=paste0("#", team_colors("Chicago Cubs")), name=NULL)+
+											scale_color_manual(breaks=names(j), labels=plot_labels, values=paste0("#", team_colors("Chicago Cubs")), name=NULL)+
+											labs(x="Number of Citations", y="Number of Papers\nPublished in 2015") +
+ 											my_theme +
+ 											theme(legend.position = c(0.8,0.9))
+
+################################################################################################
+
+ggdraw() +
+	draw_plot(time_course, x=0,y=0.66,width=1,height=0.30) +
+	draw_plot(altmetric_plot, x=0,y=0.33,1,0.30) +
+	draw_plot(citation_plot, x=0,y=0.0,1,0.30) +
+	draw_plot_label(c("A", "B", "C"), x=c(0,0,0), y=c(1.00,0.66,0.33), size=12) +
+	ggsave('figures/figure1.tiff', width=8.4, height=14, units="cm") +
+	ggsave('figures/figure1.png', width=8.4, height=14, units="cm")
